c8d0a47dcc5faf5e45d16d696b5b825be60f2d8f
jo chen <jochen10518@outlook.com>
2021-04-19 21:25:06 +0800

    Update settings.py

--- fandom_crawler/settings.py
+++ fandom_crawler/settings.py
@@ -7,10 +7,10 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'attr_by_house'
+BOT_NAME = 'fandom'
 
-SPIDER_MODULES = ['attr_by_house.spiders']
-NEWSPIDER_MODULE = 'attr_by_house.spiders'
+SPIDER_MODULES = ['fandom.spiders']
+NEWSPIDER_MODULE = 'fandom.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
